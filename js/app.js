2c03adafc2c6f3f0d334f8d2b1059a883bd78666
Eric Clota <eclota@infoselfgroup.local>
2021-06-15 17:50:34 +0200

    Paginador amb iterador de Javascript funcionant

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 const resultat = document.querySelector("#resultat");
 const formulari = document.querySelector("#formulari");
+const paginacio = document.querySelector("#paginacio");
+const registresPerPagina = 50;
+//Variable amb el total de pagines
+//Es calcularà cada cop que fem una petició a l'API
+let totalPagines = 0;
+//Es un generator de javascript
+let iterador;
 
 window.onload = ()=>{
     formulari.addEventListener('submit',validarForm);
@@ -16,6 +23,9 @@ function validarForm(e){
         //Fi funció
         return;
     }
+
+    //Si no està buit fem la consulta 
+    buscarImatge(termeSearch);
 }
 
 function mostrarAlerta(missatge){
@@ -43,4 +53,100 @@ function mostrarAlerta(missatge){
         },5000);
     }
     
+}
+
+function buscarImatge(terme){
+    const key='7058939-4555f72e178817d994d21d7d5';
+    const url = `https://pixabay.com/api/?key=${key}&q=${terme}&per_page=${registresPerPagina}`;
+
+    fetch(url)
+    .then((resultat)=>{
+        //Agafem el resultats que retorna i indiquem que els converteixi a JSON
+        return resultat.json();
+    })
+    .then((resultatJSON) => {
+        //Ens calcularà el total de pàgines que tenim
+        totalPagines = calcularPagines(resultatJSON.totalHits);
+        //Recuperem els resultats convertits JSON i treballem amb ells en aquest cas els enviem a una funció per mostrar les imatges
+        mostrarImatge(resultatJSON.hits);
+    })
+}
+
+function calcularPagines(total){
+    //Agafem el falor de la divisió del total de pàgines que tenim entre el número d'elemements a mostrar per pagina
+    //Fem u Math.ceul perquè arrodoneixi cap amunt
+    //Convertim el resultat en Int.
+    return parseInt(Math.ceil(total/registresPerPagina));
+}
+
+//Generador que registrarà la quantitat d'elements acord les pàgines
+function *crearPaginador(total){
+
+    for(let i=1; i<=total; i++){
+
+        yield(i);
+    }
+}
+
+function mostrarImatge(resultats){
+    //Buidem la llista de resultats que esten nmostrant en cas de que hi hagi algun
+    while(resultat.firstChild){
+        resultat.removeChild(resultat.firstChild);
+    }
+
+    //Iterem per cada imatge que hi ha dins de hits del resultat
+    resultats.forEach((imatge) =>{
+        //Restructuring per obtenir només l'atribut previewURL
+        const {previewURL, likes, views, largeImageURL} = imatge;
+        resultat.innerHTML += `
+        <div class="w-1/2 md:w-1/3 lg:w-1/4 mb-4 p-3">
+            <div class="bg-white ">
+                <img class="w-full" src=${previewURL} alt={tags} />
+                <div class="p-4">
+                    <p class="card-text">${likes} Me Gusta</p>
+                    <p class="card-text">${views} Vistas </p>
+
+                    <a href=${largeImageURL} 
+                    rel="noopener noreferrer" 
+                    target="_blank" class="bg-blue-800 w-full p-1 block mt-5 rounded text-center font-bold uppercase hover:bg-blue-500 text-white">Ver Imagen</a>
+                </div>
+            </div>
+        </div>
+        `;
+
+    });
+
+    //Netejar el paginador previ
+    while(paginacio.firstChild){
+        paginacio.removeChild(paginacio.firstChild);
+    }
+    //Generem el paginador nou
+    imprimirPaginador();
+
+}
+
+function imprimirPaginador(){
+    //Es un generator de javascript
+    iterador = crearPaginador(totalPagines);
+    //Aquest iterador tindrà tants elements com pàgines necessitem
+    //Podem accedir a fer coses a cada pàgina amb la funció .next();
+    while(true){
+        const { value, done } = iterador.next();
+
+        if(done){
+            return;
+        }
+
+        //Si no has arribat al final
+        //Genera un boto al paginador
+        const boto = document.createElement('a');
+
+        boto.href="#";
+        boto.dataset.pagina = value;
+        boto.textContent = value;
+
+        boto.classList.add('siguiente','bg-yellow-400','px-4','py-1','mr-2','font-bold','mb-2','uppercase','rounded');
+        paginacio.appendChild(boto);
+
+    }
 }
